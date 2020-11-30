da085b0f346ea527a122836ee7a611455e15c6cf
mihmin98 <mihmin98@yahoo.com>
2020-11-30 20:27:25 +0200

    created project

---
+++ src/main/java/com/techflow/techhubbackend/TechhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techflow.techhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechhubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techflow/techhubbackend/TechhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techflow.techhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
